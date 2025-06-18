dd7471795616429c75e2bfddf687de807e33c66e
^[[200~avnrbssoares~ <^[[200~robson.lopes@avntecnologia.com.br~>
2025-06-18 00:34:03 -0300

    Logins e Carta Permanencia

--- cypress/e2e/unimed.cy.js
+++ cypress/e2e/unimed.cy.js
@@ -1,9 +1,9 @@
 describe('Login Unimed', () => {
   it('passes', () => {
     cy.visit('https://servicoshom.unimeduberlandia.coop.br/')
-    cy.get('#Usuario').type('seu_usuario')
-    cy.get('#Senha').type('sua_senha')
-    cy.get('#dominio').type('seu_dominio')  
-    cy.get('.btn-block')
+    cy.get('#Usuario').type('12255613611')
+    cy.get('#Senha').type('Unimed@2029')
+    cy.get('#dominio').type('seu_dominio')
+    cy.get('.btn-block').click()
   })
 })
